551052ca3ee2f8338cbb08fef4c885ea8e7f0dc8
TarzanGarg <tarzan6239@gmail.com>
2025-06-06 23:07:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ let name = "Tarzan Garg";
 let age = 25;
 console.log(`my name is ${name} and my age is ${age}`)
 console.log("hello world")
-
+console.log("hello world")
+console.log("hello world")
+console.log("hello world")
+console.log("hello world")
 
 function add(a, b){
     return a+b;
